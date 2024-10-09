fd5700ff331478c090a36385366785f687717d08
Hamza Ali <hamza.ali1@edu.sait.ca>
2024-10-08 20:16:28 -0600

    Fixed week-4
    Added increment and decrement functions. As well as fixed styling.

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,44 +1,27 @@
 "use client"
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-const NewItem = () => {
-  const [quantity, setQuantity] = useState(1); 
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const item = { quantity };
-    console.log(item);
-    alert(`Item: Quantity: ${quantity}`);
-    setQuantity(1);
-    };
+export default function NewItem () {
+  const [quantity, setQuantity] = useState(1)
+  const increment = () => setQuantity(prev => Math.min(prev + 1, 20));
+  const decrement = () => setQuantity(prev => Math.max(prev - 1, 1));
 
   return (
-    <form onSubmit={handleSubmit} className="max-w-lg mx-auto mt-10 p-5 rounded shadow bg-slate-950">
-      <div className="mb-4">
-        
+    <main className='flex flex-col items-center'>
+      <div className='flex items-center bg-black space-x-10 p-2 rounded-md'>
+        <p className='text xl text-white'>{quantity}</p>
+          <button
+          onClick={increment}
+          disabled={quantity === 20}
+          className={`font-bold py-2 px-4 rounded ${quantity === 20 ? 'bg-gray-600' : 'bg-blue-400 hover:bg-blue-900'}`}>+
+          </button>
+          <button
+          onClick={decrement}
+          disabled={quantity === 1}
+          className={`font-bold py-2 px-4 rounded ${quantity === 1 ? 'bg-gray-600' : 'bg-blue-400 hover:bg-blue-900'}`}>-
+          </button>
       </div>
-
-      <div className="mb-4">
-        <label className="block text-slate-50 text-sm font-bold mb-2">Quantity</label>
-        <input 
-          type="number" 
-          value={quantity} 
-          onChange={(e) => setQuantity(Number(e.target.value))} 
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none focus:shadow-outline"
-          min="1" 
-          max="20" 
-          required 
-        />
-      </div>
-
-      
-
-      <button type="submit" className="bg-violet-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-        Confirm
-      </button>
-    </form>
-  );
-};
-
-export default NewItem;
+    </main>
+  )
+}
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,12 +1,11 @@
 import React from "react";
 import NewItem from './new-item'
 
-const Page = () => {
-    return (
+export default function Week4Page () {
+return (
         <main>
             <NewItem />
         </main>
     );
-}
+};
 
-export default Page;
